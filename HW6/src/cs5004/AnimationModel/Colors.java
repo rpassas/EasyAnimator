a5d7a1c58b4f2c0197eb44d922f23858482d9f38
rob <rpassas@gmail.com>
2021-04-05 20:11:00 -0400

    removed colors enum

--- HW6/src/cs5004/AnimationModel/Colors.java
+++
@@ -1,8 +0,0 @@
-package cs5004.AnimationModel;
-
-/**
- * Enum for the colors that can be represented by the shapes in text.
- */
-public enum Colors {
-  RED, BLUE, GREEN, ORANGE, PINK, YELLOW, PURPLE, BLACK, WHITE, BROWN
-}

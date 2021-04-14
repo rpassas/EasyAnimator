ef972b82ffb2a84a5851bee9b13783f255c2ab26
rob <rpassas@gmail.com>
2021-04-13 21:02:54 -0400

    minor tweaks

--- HW6/test/SVGViewTest.java
+++ HW6/test/SVGViewTest.java
@@ -1,9 +1,13 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+
 import cs5004.animator.model.AbstractShape;
+import cs5004.animator.model.AnimationModelImpl;
 import cs5004.animator.model.AvailableShapes;
 import cs5004.animator.model.Rect;
+import cs5004.animator.view.SVGView;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -13,6 +17,33 @@ import static org.junit.Assert.fail;
  * Tests cases for the SVGView.
  */
 public class SVGViewTest {
+  private SVGView view1;
+  private SVGView view2;
+  private AnimationModelImpl model;
+  private String toh;
 
+  @Before
+  public void setup() {
+    this.model = new AnimationModelImpl();
+    this.toh = "toh-5.txt";
+  }
+
+  @Test
+  public void testConstructor() {
+    //Testing the constructor 1 with X/Y width and Height
+    SVGView toh1 = new SVGView(model, toh);
+    assertEquals();
+  }
+
+  @Test
+  public void testInvalidConstructor() {
 
 }
+
+//SVG and Text Views do not have methods that return anything rn
+  @Test
+  public void testOutput() {
+
+  }
+
+
